26041943abad6891d9f056a8a3b68a084c04e6f8
Al B <bacx.al@gmail.com>
2013-08-20 11:28:05 -0500

    cambios en page - revisar
    -   <?php while ( have_posts() ) : the_post(); ?>
    -
    -<?php get_template_part( 'content', 'page' ); ?>
    -
    -
    -   <?php endwhile; // end of the loop. ?>

--- page.php
+++ page.php
@@ -15,12 +15,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
-
-<?php get_template_part( 'content', 'page' ); ?>
-
-
-			<?php endwhile; // end of the loop. ?>
+BIENVENIDOS
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
